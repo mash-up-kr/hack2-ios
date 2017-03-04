993ec2bd97a9fd91b835be74017fb72bc83ab4eb
seolheelee <rain00723@gmail.com>
2017-03-04 19:30:09 +0900

    기본 다음 storyboard 화면 호출 구조

--- hack2/hack2/MainVC.swift
+++ hack2/hack2/MainVC.swift
@@ -21,15 +21,9 @@ class MainVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func gameStartButtonDidClick(_ sender: Any) {
+        let gameVC = UIStoryboard(name: "GameVC", bundle: nil).instantiateViewController(withIdentifier: "GameVC")
+        self.navigationController?.pushViewController(gameVC, animated: true)
     }
-    */
 
 }
