13f6e8bf6406cca26a7a07dc80469d4062ef5ae2
yojkim <youthful2016@gmail.com>
2017-03-04 18:44:46 +0900

    ViewController 기본 세팅중

---
+++ hack2/hack2/GameVC.swift
@@ -0,0 +1,17 @@
+//
+//  GameVC.swift
+//  hack2
+//
+//  Created by yojkim on 2017. 3. 4..
+//  Copyright © 2017년 MashUp. All rights reserved.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
