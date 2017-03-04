81b10a9a4219796ccc28569e382f1f37daa0d722
yojkim <youthful2016@gmail.com>
2017-03-04 20:54:22 +0900

    자주 사용하는 색상 UIColor Extension으로 추가

---
+++ hack2/hack2/UIColor.swift
@@ -0,0 +1,24 @@
+//
+//  UIColor.swift
+//  hack2
+//
+//  Created by yojkim on 2017. 3. 4..
+//  Copyright © 2017년 MashUp. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    struct MainColor {
+        static let LightGreen = UIColor(red:0.27, green:0.89, blue:0.82, alpha:1.0)
+        static let LightBlue = UIColor(red:0.35, green:0.25, blue:0.82, alpha:1.0)
+    }
+    
+    struct SubColor {
+        static let Black = UIColor(red:0.26, green:0.26, blue:0.26, alpha:1.0)
+        static let Purple = UIColor(red:0.21, green:0.18, blue:0.38, alpha:1.0)
+        static let DarkBlue = UIColor(red:0.30, green:0.44, blue:0.62, alpha:1.0)
+    }
+    
+}
