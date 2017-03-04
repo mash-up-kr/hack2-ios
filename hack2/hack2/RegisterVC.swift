68e0d51b2a8e451f2807bae422b2910609802a32
seolheelee <rain00723@gmail.com>
2017-03-05 00:48:59 +0900

    회원가입 - 아이디, 비밀번호 입력 UI 구현

--- hack2/hack2/RegisterVC.swift
+++ hack2/hack2/RegisterVC.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2017년 MashUp. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RegisterVC: UIViewController {
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var contentViewBottomConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        NotificationCenter.default.addObserver(self, selector: #selector(RegisterVC.keyboardWillChangeFrame(notification:)), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(RegisterVC.keyboardWillHide(notification:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWillChangeFrame(notification: NSNotification) {
+        guard let keyboardHeght = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height else {
+            return
+        }
+        contentViewBottomConstraint.constant = keyboardHeght + 40
+        
+        let animations = {
+            self.view.setNeedsLayout()
+            self.view.layoutIfNeeded()
+        }
+        UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseInOut, animations: animations, completion: nil)
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        contentViewBottomConstraint.constant = 88
+        
+        let animations = {
+            self.view.setNeedsLayout()
+            self.view.layoutIfNeeded()
+        }
+        UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseInOut, animations: animations, completion: nil)
+    }
+
+}
